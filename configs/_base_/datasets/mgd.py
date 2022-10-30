d38901014d3ee86391fc7e0ce1fb4f8673d94a96
Srikar <msy290@nyu.edu>
2022-10-30 17:52:48 -0400

    add config for mgd faster rcnn

---
+++ configs/_base_/datasets/mgd.py
@@ -0,0 +1,63 @@
+# dataset settings
+dataset_type = "GunDataset"
+dataset_root = "data/mgd/"
+
+
+img_norm_cfg = dict(
+    mean=[106.15, 103.56, 101.75], std=[71.933, 72.015, 72.234], to_rgb=True
+)
+
+
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            # dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+
+data = dict(
+    samples_per_gpu = 2,
+    workers_per_gpu = 2,
+    train = dict(
+        type = dataset_type,
+        data_root = dataset_root,
+        ann_file = "annotation_detection/annotations_train.json",
+        img_prefix = "images/",
+        pipeline = train_pipeline
+    ),
+    val = dict(
+        type = dataset_type,
+        data_root = dataset_root,
+        ann_file = "annotation_detection/annotations_val.json",
+        img_prefix = "images/",
+        pipeline = test_pipeline
+    ),
+    test = dict(
+        type = dataset_type,
+        data_root = dataset_root,
+        ann_file = "annotation_detection/annotations_test.json",
+        img_prefix = "images/",
+        pipeline = test_pipeline
+    )
+)
