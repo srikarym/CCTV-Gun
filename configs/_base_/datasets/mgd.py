8ba3442662f61889ff132da8859b056d46c3255d
Srikar <msy290@nyu.edu>
2022-10-30 18:24:28 -0400

    update image norm

--- configs/_base_/datasets/mgd.py
+++ configs/_base_/datasets/mgd.py
@@ -3,12 +3,6 @@ dataset_type = "GunDataset"
 dataset_root = "data/mgd/"
 
 
-img_norm_cfg = dict(
-    mean=[106.15, 103.56, 101.75], std=[71.933, 72.015, 72.234], to_rgb=True
-)
-
-
-
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
